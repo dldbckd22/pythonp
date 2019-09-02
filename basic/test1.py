e16ddcfd86aa8c04abc0e81470a1fbab0527667b
dldbckd22 <dldbckd22@naver.com>
2019-09-02 12:51:09 +0900

    20190902last

--- basic/test1.py
+++ basic/test1.py
@@ -1,1 +1,18 @@
 print('hi-')
+a=3
+b=4
+print(a+b)
+print(a-b)
+print(a*b)
+s='py\nthon'
+s1="python"
+s2="python's"
+s3='"python"'
+s4='''python'''
+s5="""python"""
+s6='p    y'
+s7='''p
+        y'''
+head=3
+tail=' is fun'
+print(head+tail)
